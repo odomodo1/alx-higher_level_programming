2fd3f39128fdb55f36951b61bf95c0e1e6a06543
odomodo1 <odomodo19@gmail.com>
2023-03-15 17:43:12 -0700

    Multiply by using map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return (None)
-
-    return (max(a_dictionary, key=a_dictionary.get))
+def multiply_list_map(my_list=[], number=0):
+    return (list(map((lambda i: i * number), my_list)))
