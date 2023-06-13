9e0ec9a16c0c6587c9b585e4ffe4d43850a52f73
odomodo1 <odomodo19@gmail.com>
2023-06-13 23:57:05 +0000

    Fix more errors

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
+  let pi = 0;
+  while (pi < x) {
     console.log('X'.repeat(x));
-    i++;
+    pi++;
   }
 }
